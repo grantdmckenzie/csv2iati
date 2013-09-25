14509cf03570a17fe4c87ae86ad05fd57ef7ddd5
Grant McKenzie <grantdmckenzie@gmail.com>
2013-09-25 14:07:06 -0700

    small tweaks

--- handler_registeruser.php
+++ handler_registeruser.php
@@ -5,6 +5,7 @@
     $sql="SELECT * FROM users WHERE username='".pg_escape_string($_POST['username'])."'";
     $result=pg_query($sql);
     $count=pg_num_rows($result);
+
     if ($count == 0) { 
       $_POST['password'] = md5($_POST['password']);
       $result=pg_insert($db, 'users', $_POST);
